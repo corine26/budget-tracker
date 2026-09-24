111b90c07e5a148773c735e6a05c0e9a717577fb
corine <corinedino8@gmail.com>
2026-09-24 14:57:36 +0800

    Initial Laravel project setup

---
+++ app/Http/Controllers/BudgetEntryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BudgetEntry;
+use Illuminate\Http\Request;
+
+class BudgetEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BudgetEntry $budgetEntry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BudgetEntry $budgetEntry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BudgetEntry $budgetEntry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BudgetEntry $budgetEntry)
+    {
+        //
+    }
+}
